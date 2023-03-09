90cbb53b5c5f86d3681856fb0c5b2262e4cba656
Lauro <lauro.goncalves-da-rocha@datadome.co>
2023-03-09 15:56:25 +0100

    removing comments and unused settings

--- kong/plugins/datadome/handler.lua
+++ kong/plugins/datadome/handler.lua
@@ -1,24 +1,10 @@
--- If you're not sure your plugin is executing, uncomment the line below and restart Kong
--- then it will throw an error which indicates the plugin is being loaded at least.
-
---assert(ngx.get_phase() == "timer", "The world is coming to an end!")
-
----------------------------------------------------------------------------------------------
--- In the code below, just remove the opening brackets; `[[` to enable a specific handler
---
--- The handlers are based on the OpenResty handlers, see the OpenResty docs for details
--- on when exactly they are invoked and what limitations each handler has.
----------------------------------------------------------------------------------------------
-
-
-
 local plugin = {
   PRIORITY = 1000, -- set the plugin priority, which determines plugin execution order
   VERSION = "0.1",
 }
 
 ---------------------------------------------------------------------------------------------
-------------------------------------- DATADOME SECTION --------------------------------------
+------------------------------------- DATADOME CONFIG ---------------------------------------
 ---------------------------------------------------------------------------------------------
 local DATADOME_API_ENDPOINT = "api.datadome.co"
 local DATADOME_API_KEY = ""
@@ -33,6 +19,9 @@ local DATADOME_API_CONFIG = {
     uriRegexExclusion = "\\.avi|\\.flv|\\.mka|\\.mkv|\\.mov|\\.mp4|\\.mpeg|\\.mpg|\\.mp3|\\.flac|\\.ogg|\\.ogm|\\.opus|\\.wav|\\.webm|\\.webp|\\.bmp|\\.gif|\\.ico|\\.jpeg|\\.jpg|\\.png|\\.svg|\\.svgz|\\.swf|\\.eot|\\.otf|\\.ttf|\\.woff|\\.woff2|\\.css|\\.less|\\.js$"
 }
 
+---------------------------------------------------------------------------------------------
+----------------------------------- DATADOME FUNCTIONS --------------------------------------
+---------------------------------------------------------------------------------------------
 local function getClientIdAndCookiesLength(request_headers)
   local cookie = request_headers["cookie"] or ""
   local len = string.len(cookie)
@@ -130,53 +119,12 @@ local function addRequestHeaders(api_response_headers)
       end
   end
 end
+
+---------------------------------------------------------------------------------------------
+-------------------------- DATADOME EXECUTION : access phase --------------------------------
+-- Executed for every request from a client and before it is being proxied to the upstream --
 ---------------------------------------------------------------------------------------------
 
--- do initialization here, any module level code runs in the 'init_by_lua_block',
--- before worker processes are forked. So anything you add here will run once,
--- but be available in all workers.
-
-
-
--- handles more initialization, but AFTER the worker process has been forked/created.
--- It runs in the 'init_worker_by_lua_block'
-function plugin:init_worker()
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'init_worker' handler")
-
-end --]]
-
-
-
---[[ runs in the 'ssl_certificate_by_lua_block'
--- IMPORTANT: during the `certificate` phase neither `route`, `service`, nor `consumer`
--- will have been identified, hence this handler will only be executed if the plugin is
--- configured as a global plugin!
-function plugin:certificate(plugin_conf)
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'certificate' handler")
-
-end --]]
-
-
-
---[[ runs in the 'rewrite_by_lua_block'
--- IMPORTANT: during the `rewrite` phase neither `route`, `service`, nor `consumer`
--- will have been identified, hence this handler will only be executed if the plugin is
--- configured as a global plugin!
-function plugin:rewrite(plugin_conf)
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'rewrite' handler")
-
-end --]]
-
-
-
--- runs in the 'access_by_lua_block'
---https://github.com/Kong/kong/blob/master/kong/pdk/service/request.lua
 function plugin:access(plugin_conf)
   kong.log.debug("[LGR] ---------------------------------------")
   kong.log.debug("[LGR] PHASE = "..ngx.get_phase())
@@ -259,11 +207,13 @@ function plugin:access(plugin_conf)
       body = stringify(body),
       headers = datadomeHeaders
   }
+
+  -- call Datadome
   local httpc = require("resty.http").new()
   httpc:set_timeout(DATADOME_API_CONFIG.timeout)
-
   local res, err = httpc:request_uri(api_protocol .. DATADOME_API_ENDPOINT .. "/validate-request", options)
 
+  -- check errors
   kong.log.debug("[LGR] ---------------------------------------")
   if err ~= nil then
       if err == "timeout" then
@@ -277,7 +227,7 @@ function plugin:access(plugin_conf)
     kong.log.debug("[LGR] Error nil")
   end
 
-  
+  -- check response
   local status = res.status
   kong.log.debug("[LGR] status ="..status)
 
@@ -306,36 +256,6 @@ function plugin:access(plugin_conf)
     addResponseHeaders(api_response_headers)
   end
 
-
 end
 
-
--- runs in the 'header_filter_by_lua_block'
-function plugin:header_filter(plugin_conf)
-
-  -- your custom code here, for example;
-  kong.response.set_header(plugin_conf.response_header, "this is on the response")
-  kong.response.set_header("X-DataDome-Res-Lauro", "this is on the response created by Lauro")
-end --]]
-
-
---[[ runs in the 'body_filter_by_lua_block'
-function plugin:body_filter(plugin_conf)
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'body_filter' handler")
-
-end --]]
-
-
---[[ runs in the 'log_by_lua_block'
-function plugin:log(plugin_conf)
-
-  -- your custom code here
-  kong.log.debug("saying hi from the 'log' handler")
-
-end --]]
-
-
--- return our plugin object
 return plugin
